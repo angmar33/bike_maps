b5aa349ec6d45a8a744981bc3f9c420520b29f92
angmar33 <paultsvilyuk@gmail.com>
2021-04-21 22:10:06 +0300

    update: swagger added.

--- bikemaps/bikemaps/urls.py
+++ bikemaps/bikemaps/urls.py
@@ -16,6 +16,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+swagger_view = get_schema_view(
+   openapi.Info(
+      title="Swagger UI",
+      default_version='0.1'
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
+    path('api/swagger/',
+         swagger_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
 ]
